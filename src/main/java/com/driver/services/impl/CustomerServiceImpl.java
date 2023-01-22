626a1dba58534d2c2f40867f07f40b9387ccc65e
Sourabh <createownpath111@gmail.com>
2023-01-22 12:59:53 +0530

    code6

--- src/main/java/com/driver/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/driver/services/impl/CustomerServiceImpl.java
@@ -84,6 +84,7 @@ public class CustomerServiceImpl implements CustomerService {
 
 			TripBooking tripBooking = tripBookingRepository2.findById(tripId).get();
 			tripBooking.setStatus(TripStatus.CANCELED);
+			tripBooking.setBill(0);
 
 			Driver driver = tripBooking.getDriver();
 			driver.getCab().setAvailable(true);
