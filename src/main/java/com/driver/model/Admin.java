e6e43dfe116efa7413ae68909e5a8cfca96cfafb
Sourabh <createownpath111@gmail.com>
2023-01-22 15:43:22 +0530

    code9

--- src/main/java/com/driver/model/Admin.java
+++ src/main/java/com/driver/model/Admin.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class Admin{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int adminId;
 
     private String username;
