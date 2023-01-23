385ff210c06d8d62d2e4c25294b6cb0aa30b2428
Sourabh <createownpath111@gmail.com>
2023-01-23 23:53:37 +0530

    code13

--- src/main/java/com/driver/model/Driver.java
+++ src/main/java/com/driver/model/Driver.java
@@ -1,6 +1,7 @@
 package com.driver.model;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -19,7 +20,7 @@ public class Driver {
     private Cab cab;
 
     @OneToMany(mappedBy = "driver",cascade = CascadeType.ALL)
-    private List<TripBooking> tripBookingList;
+    private List<TripBooking> tripBookingList = new ArrayList<>();
 
     public Driver(String mobile, String password) {
         this.mobile = mobile;
